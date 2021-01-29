13ddbef5f1f963f3ef9d3f7115061be808825653
Barbara Peric <b.peric10@gmail.com>
2021-01-29 15:45:42 +0100

    FIX: starter code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+
+function ColorfulBorder() {
+  return (
+    <React.Fragment>
+      <ul className='border-container'>
+        <li className='border-item' style={{ background: 'var(--red)' }} />
+        <li className='border-item' style={{ background: 'var(--blue)' }} />
+        <li className='border-item' style={{ background: 'var(--pink)' }} />
+        <li className='border-item' style={{ background: 'var(--yellow)' }} />
+        <li className='border-item' style={{ background: 'var(--aqua)' }} />
+      </ul>
+    </React.Fragment>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorfulBorder />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
