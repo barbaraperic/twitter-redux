66135fd81d0bdbd3ef535fadf87cd427a11d0482
Barbara Peric <b.peric10@gmail.com>
2021-01-30 15:53:47 +0100

    FEAT: added tweets reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,7 +3,10 @@ import { RECEIVE_USERS } from '../actions/users'
 export const users = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_USERS :
-      return {...action.users}
+      return {
+        ...state,
+        ...action.users
+      }
     default:
       return state
   }
